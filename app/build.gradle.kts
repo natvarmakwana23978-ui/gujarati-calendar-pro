92d67b5f0a99dc8240b889bfaf18ac763e143656
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-08 19:04:50 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,17 @@ android {
             isMinifyEnabled = false
         }
     }
+    
+    // ✅ આ નવું ઉમેરો:
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    // ✅ આ પણ ઉમેરો:
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
