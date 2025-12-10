b31b0828c878f8d59331f6143ce900b69b6b59b7
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-10 21:47:42 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,27 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-// allprojects { repositories { ... } } બ્લોક અહીંથી કાઢી નાખવામાં આવ્યો છે.
+// જો તમારે repositories અહીં જોડવી હોય તો
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // આ લાઈન ઉમેરો
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // આ લાઈન પણ ઉમેરો
+    }
+}
