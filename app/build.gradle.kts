53a22f5558de364cff19d2d4ec98f74cbceb31c8
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-10 21:27:35 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    // ✅ Swiss Ephemeris લાઇબ્રેરી (આ લાઈન ઉમેરી છે)
+    implementation("org.astrolabsoftware:swisseph:2.10.03")
 }
