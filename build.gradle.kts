92469bb7dbe35abb48d7c8cef982ff5b012328ad
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-10 22:07:39 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,3 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
-// જો તમારે repositories અહીં જોડવી હોય તો
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") } // આ લાઈન ઉમેરો
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") } // આ લાઈન પણ ઉમેરો
-    }
-}
