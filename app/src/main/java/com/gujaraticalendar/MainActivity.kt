c0ce7d9cbae0de01dc3d9942b5018e8939aadd0b
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-11 12:33:04 +0530

    Update MainActivity.kt

--- app/src/main/java/com/gujaraticalendar/MainActivity.kt
+++ app/src/main/java/com/gujaraticalendar/MainActivity.kt
@@ -1,25 +1,17 @@
 package com.gujaraticelendar
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // ✅ સરળ અને safe version
-        try {
-            // એકદમ સરળ UI
-            val textView = android.widget.TextView(this)
-            textView.text = "ગુજરાતી કેલેન્ડર\nએપ્લિકેશન"
-            textView.textSize = 20f
-            textView.gravity = android.view.Gravity.CENTER
-            setContentView(textView)
-            
-        } catch (e: Exception) {
-            // Error handle
-            e.printStackTrace()
-        }
+        // એકદમ સરળ
+        val textView = TextView(this)
+        textView.text = "ગુજરાતી કેલેન્ડર એપ"
+        textView.textSize = 24f
+        setContentView(textView)
     }
 }
