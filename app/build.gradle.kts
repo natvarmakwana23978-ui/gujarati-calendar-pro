93dca122086646f29d5e498672a75c74954d3e3f
natvarmakwana23978-ui <natvarmakwana23978@gmail.com>
2025-12-11 08:46:05 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,9 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
-    // Swiss Ephemeris dependency - CASE SENSITIVE ફેરફાર
-    implementation("com.github.astrolabsoftware:swisseph-android:2.10.03") // "L" કૅપિટલમાં
+    // ❌ હમણાં માટે Swiss Ephemeris નો કોઈ reference નહીં
+    // બિલકુલ આ લીટી નહીં
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
